6c3f51a0c2895813eb4394e1bba78e671c790094
Sauransh Bhardwaj <sbhardwaj1418@gmail.com>
2024-08-26 14:17:39 +0530

    commit

--- components/Approach.tsx
+++ components/Approach.tsx
@@ -7,17 +7,15 @@ const Approach = () => {
   return (
     <section className="w-full py-20">
       <h1 className="heading">
-        My <span className="text-purple">approach</span>
+        What's <span className="text-purple">Cooking </span>rn?
       </h1>
       {/* remove bg-white dark:bg-black */}
       <div className="my-20 flex flex-col lg:flex-row items-center justify-center w-full gap-4">
         {/* add des prop */}
         <Card
-          title="Planning & Strategy"
+          title="Waitlist and Launching the Hype Rocket"
           icon={<AceternityIcon order="Phase 1" />}
-          des="We'll collaborate to map out your website's goals, target audience, 
-          and key functionalities. We'll discuss things like site structure, 
-          navigation, and content requirements."
+          des=""
         >
           <CanvasRevealEffect
             animationSpeed={5.1}
@@ -26,11 +24,9 @@ const Approach = () => {
           />
         </Card>
         <Card
-          title="Development & Progress Update"
+          title="V1 launch and BUIDLing your first moon bags"
           icon={<AceternityIcon order="Phase 2" />}
-          des="Once we agree on the plan, I cue my lofi playlist and dive into
-          coding. From initial sketches to polished code, I keep you updated
-          every step of the way."
+          des=""
         >
           <CanvasRevealEffect
             animationSpeed={3}
@@ -48,11 +44,9 @@ const Approach = () => {
           {/* <div className="absolute inset-0 [mask-image:radial-gradient(400px_at_center,white,transparent)] bg-black/50 dark:bg-black/90" /> */}
         </Card>
         <Card
-          title="Development & Launch"
+          title="V2 Mobile App Launch"
           icon={<AceternityIcon order="Phase 3" />}
-          des="This is where the magic happens! Based on the approved design, 
-          I'll translate everything into functional code, building your website
-          from the ground up."
+          des=""
         >
           <CanvasRevealEffect
             animationSpeed={3}
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,57 +1,43 @@
 import { FaLocationArrow } from "react-icons/fa6";
-
 import { socialMedia } from "@/data";
 import MagicButton from "./MagicButton";
 
 const Footer = () => {
   return (
-    <footer className="w-full pt-20 pb-10" id="contact">
+    <footer className="relative w-full pt-20 pb-10" id="contact">
       {/* background grid */}
       <div className="w-full absolute left-0 -bottom-72 min-h-96">
         <img
           src="/footer-grid.svg"
           alt="grid"
-          className="w-full h-full opacity-50 "
+          className="w-full h-full opacity-50"
         />
       </div>
 
       <div className="flex flex-col items-center">
-        <h1 className="heading lg:max-w-[60vw] leading">
-          are you <span className="text-purple">ready? </span>
+        <h1 className="heading lg:max-w-[60vw] leading text-center">
+          JOIN THE ASTRONAUT CORE OR{" "}
+          <span className="text-purple">GET REKT</span>
         </h1>
-        <p className="text-white-200 md:mt-10 my-5 text-center">
-          vidyaverse is the entry point for the next billion users in web3. join
-          this rocket ship!
-        </p>
-        <a
-          href="https://amzn.in/d/aI6OWvy"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <MagicButton
-            title="web3 book"
-            icon={<FaLocationArrow />}
-            position="right"
-          />
-        </a>
-      </div>
-      <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
-        <p className="md:text-base text-sm md:font-normal font-light">
-          Copyright © 2024 VidyaVerse
-        </p>
 
-        <div className="flex items-center md:gap-3 gap-6">
+        {/* Social media icons centered below the heading */}
+        <div className="flex flex-wrap justify-center gap-4 mt-10 mb-16">
           {socialMedia.map((info) => (
-            <div
+            <a
               key={info.id}
-              className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
+              href={info.url}
+              className="flex items-center justify-center"
             >
-              <a href={info.url}>
-                <img src={info.img} alt="icons" width={20} height={20} />
-              </a>
-            </div>
+              <div className="w-16 h-16 flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300">
+                <img src={info.img} alt="icons" width={32} height={32} />
+              </div>
+            </a>
           ))}
         </div>
+      </div>
+
+      <div className="flex flex-col md:flex-row justify-between items-center text-center md:text-base text-sm font-light">
+        <p className="md:font-normal">Copyright © 2024 VidyaVerse</p>
       </div>
     </footer>
   );
